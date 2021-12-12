88191c3789260a7fe78d9325f9d3156c41d35d25
hyunho1027 <ramanuzann@gmail.com>
2021-12-12 20:02:45 +0900

    update bc

--- agents/09.behavioral_clonning.py
+++ agents/09.behavioral_clonning.py
@@ -51,14 +51,12 @@ class Actor(torch.nn.Module):
     def __init__(self):
         super(Actor, self).__init__()
         self.fc1 = torch.nn.Linear(state_size, 128)
-        self.bn1 = torch.nn.BatchNorm1d(128)
         self.fc2 = torch.nn.Linear(128, 128)
-        self.bn2 = torch.nn.BatchNorm1d(128)
         self.action = torch.nn.Linear(128, action_size)
         
     def forward(self, state):
-        x = torch.relu(self.bn1(self.fc1(state)))
-        x = torch.relu(self.bn2(self.fc2(x)))
+        x = torch.relu(self.fc1(state))
+        x = torch.relu(self.fc2(x))
         return torch.tanh(self.action(x))
 
 # BCAgent 클래스 -> Behavioral Cloning 알고리즘을 위한 다양한 함수 정의
@@ -134,7 +132,7 @@ if __name__ == '__main__':
         for t in reversed(range(len(ret) - 1)):
             ret[t] += (1. - done[t]) * (discount_factor * ret[t+1])
         
-        # return가 0보다 큰 (state, action) pair만 학습에 사용.
+        # return이 0보다 큰 (state, action) pair만 학습에 사용.
         state, action = map(lambda x: x[ret > 0], [state, action])
         
         losses = []
