be6357d22439130362e3ea8318882ede0cc25bb3
hyunho1027 <ramanuzann@gmail.com>
2021-12-12 19:40:13 +0900

    update bc and demo location

--- agents/09.behavioral_clonning.py
+++ agents/09.behavioral_clonning.py
@@ -41,7 +41,7 @@ save_path = f"./saved_models/{game}/BC/{date_time}"
 load_path = f"./saved_models/{game}/BC/20211128183733"
 
 # Demonstration 경로
-demo_path = "./demo/KartAgent.demo"
+demo_path = "../demo/KartAgent.demo"
 
 # 연산 장치
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -51,12 +51,14 @@ class Actor(torch.nn.Module):
     def __init__(self):
         super(Actor, self).__init__()
         self.fc1 = torch.nn.Linear(state_size, 128)
+        self.bn1 = torch.nn.BatchNorm1d(128)
         self.fc2 = torch.nn.Linear(128, 128)
+        self.bn2 = torch.nn.BatchNorm1d(128)
         self.action = torch.nn.Linear(128, action_size)
         
     def forward(self, state):
-        x = torch.relu(self.fc1(state))
-        x = torch.relu(self.fc2(x))
+        x = torch.relu(self.bn1(self.fc1(state)))
+        x = torch.relu(self.bn2(self.fc2(x)))
         return torch.tanh(self.action(x))
 
 # BCAgent 클래스 -> Behavioral Cloning 알고리즘을 위한 다양한 함수 정의
