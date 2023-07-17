7228719a31f6774368c7ce119c7e0ee58df1dd71
Yumin Park <qkrdbals1630@gmail.com>
2023-07-18 02:43:03 +0900

    Update Dodge Project

---
+++ unity_project/Dodge/Assets/Dodge/Scripts/BallScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    GameObject Agent;
+    float ball_speed;
+    float ball_random;
+    float board_radius;
+
+    public void SetBall(GameObject Agent_, float ball_speed_, float ball_random_, float board_radius_)
+    {
+        Agent = Agent_;
+        ball_random = ball_random_;
+        ball_speed = ball_speed_;
+        board_radius = board_radius_ - 0.55f;
+
+        RandomBall();
+    }
+
+    public void RandomBall()
+    {
+        float theta = Random.Range(0, 2 * Mathf.PI);
+        transform.localPosition = new Vector3(board_radius * Mathf.Cos(theta), 0.25f, board_radius * Mathf.Sin(theta));
+
+        float randAngle = Mathf.Atan2(Agent.transform.localPosition.z - transform.localPosition.z, Agent.transform.localPosition.x - transform.localPosition.x) + Random.Range(-ball_random, ball_random);
+        float randSpeed = ball_speed + Random.Range(-0.5f * ball_random, 0.5f * ball_random);
+
+        Rigidbody rig = GetComponent<Rigidbody>();
+        rig.velocity = new Vector3(randSpeed * Mathf.Cos(randAngle), 0, randSpeed * Mathf.Sin(randAngle));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("wall"))
+        {
+            RandomBall();
+        }
+    }
+}
